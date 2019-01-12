7e61a5f8835746954047d68ca7fb509c71dfcb66
clowweiner <cecilia.lowweiner@gmail.com>
2019-01-12 16:03:56 -0500

    Exploring Data

--- taxi_data.R
+++ taxi_data.R
@@ -1,7 +1,20 @@
 library(tidyverse)
 library(ggplot2)
 
+#read in data
 taxi_df <- read.csv("/Users/cecilialow-weiner/Desktop/Taxi Data/2017_Green_Taxi_Trip_Data.csv")
-head(taxi_df)
 
+# explore data structure and names
+head(taxi_df)
 str(taxi_df)
+head(taxi_df$PULocationID)
+names(taxi_df)
+
+# create frequency table for distance
+df <- as.data.frame(table(sort(taxi_df$trip_distance)))
+colnames(df) <- c("Distance", "Frequency")
+
+# filter for trips over a mile
+df$Distance <- as.numeric(df$Distance)
+mile_trips <- df %>% filter(Distance >= 1)
+sum(mile_trips)
